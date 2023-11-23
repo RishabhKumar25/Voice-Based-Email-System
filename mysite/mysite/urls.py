edea44e41d4fe6e794faf0955870ede9d4cb3d98
Rishabh <karnrishabh25@gmail.com>
2023-11-23 15:40:01 +0530

    initial commit

---
+++ mysite/mysite/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import re_path as url
+
+from django.urls import include
+
+urlpatterns = [
+    url(r'^admin/$', admin.site.urls),
+    url(r'^',include('homepage.urls')),
+    
+]
